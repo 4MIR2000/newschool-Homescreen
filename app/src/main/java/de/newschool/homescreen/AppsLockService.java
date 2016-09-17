f33811072cecaaf12c5e54c8e34efff0b2f22fca
Amir <taghizadegan.amir@yahoo.de>
2016-09-18 00:51:42 +0200

    nothing

---
+++ app/src/main/java/de/newschool/homescreen/AppsLockService.java
@@ -0,0 +1,11 @@
+package de.newschool.homescreen;
+
+import android.app.Service;
+
+/**
+ * Created by ASUS on 11.09.2016.
+ */
+public class AppsLockService {
+
+
+}
---
+++ app/src/main/java/de/newschool/homescreen/LockedAppDetail.java
@@ -0,0 +1,10 @@
+package de.newschool.homescreen;
+
+/**
+ * Created by ASUS on 12.09.2016.
+ */
+public class LockedAppDetail{
+
+
+
+}
