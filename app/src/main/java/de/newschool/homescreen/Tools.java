99f13a1375dba43db62233b1d3bb49b677fd91d8
Amir <taghizadegan.amir@yahoo.de>
2016-09-18 01:03:17 +0200

    added getReligion() methode to get the religion of student from txt

--- app/src/main/java/de/newschool/homescreen/Tools.java
+++ app/src/main/java/de/newschool/homescreen/Tools.java
@@ -4,6 +4,14 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.Environment;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 class Tools {
     public static Bitmap getdrawableToBitmap(Drawable drawable) {
@@ -17,4 +25,31 @@ class Tools {
 
         return bitmap;
     }
+
+    public static String getReligion(){
+            File root = new File(Environment.getExternalStorageDirectory()+File.separator+".NewSchool"+File.separator+"Benutzer");
+            File religion_file = new File(root+File.separator+"r.txt");
+
+            FileInputStream fis = null;
+            InputStreamReader isr = null;
+            BufferedReader br = null;
+
+            if(religion_file.exists()) {
+                try {
+
+                    fis = new FileInputStream(religion_file);
+                    isr = new InputStreamReader(fis);
+                    br = new BufferedReader(isr);
+
+                    return br.readLine();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            return null;
+        }
 }
